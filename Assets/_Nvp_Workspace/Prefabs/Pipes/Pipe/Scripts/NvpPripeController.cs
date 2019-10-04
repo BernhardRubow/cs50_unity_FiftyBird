56bc2c2c5a2ff24da04a4ee57d20c16b6e08c8a1
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2019-10-04 18:38:52 +0200

    Added Object pool for Pipes and reorganised Workspace for Nvp

---
+++ Assets/_Nvp_Workspace/Prefabs/Pipes/Pipe/Scripts/NvpPripeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NvpPripeController : MonoBehaviour, IPoolItem
+{
+    // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private System.Action<GameObject> _returnToPoolAction;
+
+
+
+
+
+    // +++ IPoolItem implementation +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public void SetReturnAction(Action<GameObject> returnToPoolAction)
+    {
+        if (_returnToPoolAction == null) _returnToPoolAction = returnToPoolAction;
+    }
+
+
+
+
+    // +++ class methods ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    void ReturnToPool()
+    {
+        _returnToPoolAction(this.gameObject);
+    }
+}
---
+++ Assets/_Nvp_Workspace/Scripts/NvpPools/NvpPipePool.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NvpPipePool : MonoBehaviour, IGameObjectPool
+{
+    // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [SerializeField] private GameObject _pipePrefab;
+    [SerializeField] private int _prefillPoolCount;
+
+    private Queue<GameObject> _pool;
+    private static NvpPipePool INSTANCE;
+
+
+
+
+    // +++ life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    void Start()
+    {
+        NvpPipePool.INSTANCE = this;
+        _pool = new Queue<GameObject>();
+        if (_prefillPoolCount > 1) PreparePool(_prefillPoolCount);
+    }
+
+
+
+
+    // +++ IGameObjectPool Implementation +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public GameObject GetFromPool()
+    {
+        if(this._pool.Count == 0)
+        {
+            AddToPool(1);
+        }
+
+        var pipe = this._pool.Dequeue();
+        
+        pipe.GetComponent<IPoolItem>().SetReturnAction(ReturnToPool);
+
+        return pipe;
+    }
+
+
+    public void ReturnToPool(GameObject poolItem)
+    {
+        poolItem.SetActive(false);
+        this._pool.Enqueue(poolItem);
+    }
+    
+    
+    
+    
+    // +++ private class methods ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void PreparePool(int n)
+    {
+        AddToPool(n);
+    }
+
+
+    private void AddToPool(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            var pipe = Instantiate(_pipePrefab);
+            pipe.gameObject.SetActive(false);
+            _pool.Enqueue(pipe);
+        }
+    }
+
+
+    public static GameObject GetPooledItem()
+    {
+        return NvpPipePool.INSTANCE.GetFromPool();
+    }
+}
